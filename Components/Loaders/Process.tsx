b68c591ab34b94f27de2c852b3b981b15f5c96fb
UmarovIkrom <ikromme13@gmail.com>
2024-05-02 23:36:36 +0500

    init: common process loader added

---
+++ Components/Loaders/Process.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import styles from "./ProcessStyles.module.scss";
+
+const Process = () => {
+  return (
+    <div className={styles["lds-ring"]}>
+      <div />
+      <div />
+      <div />
+      <div />
+    </div>
+  );
+};
+
+export default Process;
